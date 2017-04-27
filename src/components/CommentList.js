a334162c5e6ce1106dfa8dc5f69c19beb4c3e576
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-04-27 22:21:01 +0300

    rewrite CommentList to stateless component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,34 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Comment from './Comment'
 import toggleOpen from '../decorators/toggleOpen'
 
-class CommentList extends Component {
-    static defaultProps = {
-        comments: []
-    }
+function CommentList(props) {
+    const {isOpen, toggleOpen} = props
+    const linkText = isOpen ? 'hide comments' : 'show comments'
 
-    render() {
-        const {isOpen, toggleOpen} = this.props
-        const linkText = isOpen ? 'hide comments' : 'show comments'
+    return (
+        <div>
+            <a href="#" onClick={toggleOpen}>{linkText}</a>
+            {getBody(props)}
+        </div>
+    )
+}
 
-        return (
-            <div>
-                <a href="#" onClick={toggleOpen}>{linkText}</a>
-                {this.getBody()}
-            </div>
-        )
-    }
-
-    getBody() {
-        const {isOpen, comments} = this.props
-        if (!isOpen) return null
-        if (!comments.length) return <p>No comments yet</p>
-        return (
-            <ul>
-                {comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)}
-            </ul>
-        )
-    }
+function getBody({isOpen, comments = []}) {
+    if (!isOpen) return null
+    if (!comments.length) return <p>No comments yet</p>
+    return (
+        <ul>
+            {comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)}
+        </ul>
+    )
 }
 
 export default toggleOpen(CommentList)
