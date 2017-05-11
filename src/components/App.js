746125717c691d310e9053e4e17488d62f212f08
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-05-11 22:37:55 +0300

    add react-select

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import ArticleList from './ArticleList'
 import Chart from './Chart'
 import UserForm from './UserForm'
+import Select from 'react-select'
+import 'react-select/dist/react-select.css'
 
 class App extends Component {
     static propTypes = {
@@ -9,14 +11,21 @@ class App extends Component {
     };
 
     state = {
-        counter: 0
+        counter: 0,
+        selection: null
     }
 
     render() {
+        const options = this.props.articles.map(article => ({
+            label: article.title,
+            value: article.id
+        }))
         return (
             <div>
                 <UserForm />
                 <a href="#" onClick = {this.updateCounter}>update chart</a>
+                <Select options = {options} value = {this.state.selection}
+                        onChange = {this.handleSelectionChange} multi />
                 <ArticleList articles = {this.props.articles} />
                 <Chart articles={this.props.articles} key={this.state.counter} />
             </div>
@@ -29,6 +38,8 @@ class App extends Component {
             counter: this.state.counter + 1
         })
     }
+
+    handleSelectionChange = selection => this.setState({ selection })
 }
 
 export default App
