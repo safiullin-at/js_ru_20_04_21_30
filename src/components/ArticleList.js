6b028cf192cd281a9c0ed5351eaa929d50d9bc8f
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-05-11 22:04:43 +0300

    add component ref

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react'
+import {findDOMNode} from 'react-dom'
 import Article from './Article'
 import PropTypes from 'prop-types'
 import accordion from '../decorators/accordion'
 
 class ArticleList extends Component {
     componentDidMount() {
-        console.log('---', this.refs)
+        const ref = this.refs[this.props.articles[0].id]
+        console.log('---', ref, findDOMNode(ref))
     }
 
     render() {
         const {articles, toggleOpenItem, isItemOpened} = this.props
-        const elements = articles.map(article => <li key={article.id} ref = {article.id}>
+        const elements = articles.map(article => <li key={article.id}>
             <Article article={article}
                      isOpen={isItemOpened(article.id)}
-                     toggleOpen={toggleOpenItem(article.id)}/>
+                     toggleOpen={toggleOpenItem(article.id)}
+                     ref = {article.id}
+            />
         </li>)
         return (
             <ul>
